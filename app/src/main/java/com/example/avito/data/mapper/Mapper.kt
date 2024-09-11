016ea33108b4721b0039c5c93b3169a55ba7ca2e
jayelmeynak <magomed110301@yandex.ru>
2024-09-11 22:30:39 +0300

    Refactor Mapper

--- app/src/main/java/com/example/avito/data/mapper/Mapper.kt
+++ app/src/main/java/com/example/avito/data/mapper/Mapper.kt
@@ -1,14 +1,13 @@
 package com.example.avito.data.mapper
 
 import com.example.avito.data.network.models.product.ProductDto
-import com.example.avito.data.network.models.product.ProductResponse
 import com.example.avito.data.network.models.product.ProductSpecificationDto
 import com.example.avito.domain.Product
 import com.example.avito.domain.ProductSpecification
 
 class Mapper {
 
-    fun productDtoToEntity(product: ProductDto): Product{
+    fun productDtoToEntity(product: ProductDto): Product {
         return Product(
             id = product.id,
             images = product.images,
@@ -16,29 +15,27 @@ class Mapper {
             price = product.price,
             name = product.name,
             description = product.description,
-            brand = product.brand,
             category = product.category,
-            productRating = product.productRating,
-            productSpecifications = listSpecificationMapper(product.productSpecifications),
-            v = product.v
+            productSpecifications = listSpecificationMapper(product.productSpecifications)
         )
     }
 
-    fun specificationDtoToEntity(specificationDto: ProductSpecificationDto): ProductSpecification{
+    fun specificationDtoToEntity(specificationDto: ProductSpecificationDto): ProductSpecification {
         return ProductSpecification(
             key = specificationDto.key,
             value = specificationDto.value
         )
     }
-    fun listSpecificationMapper(list: List<ProductSpecificationDto>): List<ProductSpecification>{
+
+    fun listSpecificationMapper(list: List<ProductSpecificationDto>): List<ProductSpecification> {
         return list.map {
             specificationDtoToEntity(it)
         }
     }
 
-    fun listProductDtoToEntity(list: List<ProductResponse>): List<Product>{
+    fun listProductDtoToEntity(list: List<ProductDto>): List<Product> {
         return list.map {
-            productDtoToEntity(it.product)
+            productDtoToEntity(it)
         }
     }
 }
