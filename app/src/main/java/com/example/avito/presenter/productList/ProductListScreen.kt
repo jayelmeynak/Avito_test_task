c5df6328cc7010a9be417ad60ee5e3c63defb1a8
jayelmeynak <magomed110301@yandex.ru>
2024-09-12 11:03:34 +0300

    feat(ui): add category filtering to ProductListScreen
    - Introduced `CategoryRow` composable to display category options for filtering.
    - Updated `ProductListScreen` and `ScreenContent` to include category filtering functionality.
    - Categories are clickable, and products are updated based on the selected category.
    
    refactor(ui): change placeholder image in CardProduct
    
    - Replaced the placeholder image in `CardProduct` with `ic_image_search` for better visual representation.

--- app/src/main/java/com/example/avito/presenter/productList/ProductListScreen.kt
+++ app/src/main/java/com/example/avito/presenter/productList/ProductListScreen.kt
@@ -1,17 +1,23 @@
 package com.example.avito.presenter.productList
 
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -21,8 +27,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,6 +40,7 @@ import coil.ImageLoader
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.avito.R
+import com.example.avito.domain.Category
 import com.example.avito.domain.Product
 import com.example.avito.presenter.navigation.Screens
 import okhttp3.OkHttpClient
@@ -48,23 +57,70 @@ fun ProductListScreen(navController: NavController) {
     if (viewModel.isLoading.value) {
         Loading()
     } else {
-        ScreenContent(viewModel.products.value, navController)
+        ScreenContent(
+            viewModel.products.value,
+            navController,
+            viewModel.categoryList
+        ) { categoryName, index ->
+            viewModel.setCategory(categoryName, index)
+        }
     }
 }
 
 @Composable
-fun ScreenContent(productList: List<Product>, navController: NavController) {
+fun ScreenContent(
+    productList: List<Product>,
+    navController: NavController,
+    categoryList: List<Category>,
+    onClickCategory: (String, Int) -> Unit
+) {
     Scaffold { innerPadding ->
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = innerPadding.calculateTopPadding()),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            items(productList.size, key = {productList[it].id}) { index ->
-                CardProduct(product = productList[index], navController = navController)
+        Column {
+
+            CategoryRow(innerPadding, categoryList, onClickCategory)
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                items(productList.size, key = { productList[it].id }) { index ->
+                    CardProduct(product = productList[index], navController = navController)
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun CategoryRow(
+    innerPadding: PaddingValues,
+    categoryList: List<Category>,
+    onClickCategory: (String, Int) -> Unit
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = innerPadding.calculateTopPadding() + 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        categoryList.forEachIndexed { index, category ->
+            Image(
+                modifier = Modifier
+                    .size(70.dp)
+                    .clip(RoundedCornerShape(5.dp))
+                    .background(MaterialTheme.colorScheme.background)
+                    .clickable {
+                        onClickCategory(category.categoryName, index)
+                    },
+                painter = painterResource(id = category.image),
+                contentDescription = category.name,
+                contentScale = ContentScale.FillBounds
+            )
         }
     }
 }
@@ -124,7 +180,7 @@ fun ColumnScope.ProductImage(product: Product) {
         model = ImageRequest.Builder(LocalContext.current)
             .data(if (product.images.isEmpty()) R.drawable.ic_placeholder else product.images[0])
             .crossfade(true)
-            .placeholder(R.drawable.ic_placeholder)
+            .placeholder(R.drawable.ic_image_search)
             .error(R.drawable.ic_placeholder)
             .build(),
         contentDescription = "Product image",
