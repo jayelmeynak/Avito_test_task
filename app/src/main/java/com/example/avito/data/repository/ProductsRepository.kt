e0cfac4c2f7a58b27f7ec2ba512280db0aead0a8
jayelmeynak <magomed110301@yandex.ru>
2024-09-12 04:45:37 +0300

    feat(repository): add new methods for filtering and sorting products
    - Added `getProductsFilterByCategory()` to retrieve products filtered by category.
    - Added `getProductsWithPriceSort()` to retrieve products sorted by price.
    - Added `getProductsWithPriceSortAndCategory()` to retrieve products sorted by price and filtered by category.
    - Existing methods remain unchanged.

--- app/src/main/java/com/example/avito/data/repository/ProductsRepository.kt
+++ app/src/main/java/com/example/avito/data/repository/ProductsRepository.kt
@@ -25,6 +25,53 @@ class ProductsRepository {
         }
     }
 
+    suspend fun getProductsFilterByCategory(
+        category: String
+    ): Result<List<Product>> {
+        return try {
+            val response = apiService.getProductsFilterByCategory(category)
+            if (response.isSuccessful) {
+                Result.success(response.body()?.productList?.let { mapper.listProductDtoToEntity(it) }
+                    ?: emptyList())
+            } else {
+                Result.failure(Exception("Error: ${response.getErrorResponse()?.message ?: "Unknown error"}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun getProductsWithPriceSort(sort: String): Result<List<Product>> {
+        return try {
+            val response = apiService.getProductsWithPriceSort(sort)
+            if (response.isSuccessful) {
+                Result.success(response.body()?.productList?.let { mapper.listProductDtoToEntity(it) }
+                    ?: emptyList())
+            } else {
+                Result.failure(Exception("Error: ${response.getErrorResponse()?.message ?: "Unknown error"}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun getProductsWithPriceSortAndCategory(
+        sort: String,
+        category: String
+    ): Result<List<Product>> {
+        return try {
+            val response = apiService.getProductsWithPriceSortAndFilterByCategory(sort, category)
+            if (response.isSuccessful) {
+                Result.success(response.body()?.productList?.let { mapper.listProductDtoToEntity(it) }
+                    ?: emptyList())
+            } else {
+                Result.failure(Exception("Error: ${response.getErrorResponse()?.message ?: "Unknown error"}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
     suspend fun getProduct(id: String): Product {
         return mapper.productDtoToEntity(apiService.getProduct(id).product)
     }
