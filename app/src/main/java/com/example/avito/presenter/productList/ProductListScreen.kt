d3ca10d4740e577ae6208c76cc9c7bdaf21eb218
jayelmeynak <magomed110301@yandex.ru>
2024-09-12 18:49:02 +0300

    feat(ui): add price sorting functionality to ProductListScreen
    - Implemented SortView component to handle price sorting options
    - Added DropMenu for sorting selection
    - Updated ProductListScreen to support price sorting via SortView
    - Connected sorting state with ViewModel to update product list based on user selection

--- app/src/main/java/com/example/avito/presenter/productList/ProductListScreen.kt
+++ app/src/main/java/com/example/avito/presenter/productList/ProductListScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
@@ -20,11 +21,15 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -60,10 +65,16 @@ fun ProductListScreen(navController: NavController) {
         ScreenContent(
             viewModel.products.value,
             navController,
-            viewModel.categoryList
-        ) { categoryName, index ->
-            viewModel.setCategory(categoryName, index)
-        }
+            viewModel.expanded,
+            viewModel.activeSort,
+            viewModel.categoryList,
+            onClickCategory = {selectedCategory, index ->
+                viewModel.setCategory(selectedCategory, index)
+            },
+            onClickSort = { selectedSort->
+                viewModel.setSort(selectedSort)
+            }
+        )
     }
 }
 
@@ -71,8 +82,11 @@ fun ProductListScreen(navController: NavController) {
 fun ScreenContent(
     productList: List<Product>,
     navController: NavController,
+    expanded: MutableState<Boolean>,
+    activeSort: State<Int>,
     categoryList: List<Category>,
-    onClickCategory: (String, Int) -> Unit
+    onClickCategory: (String, Int) -> Unit,
+    onClickSort: (Int) -> Unit
 ) {
     Scaffold { innerPadding ->
         Column {
@@ -80,6 +94,8 @@ fun ScreenContent(
             CategoryRow(innerPadding, categoryList, onClickCategory)
 
             Spacer(modifier = Modifier.height(8.dp))
+
+            SortView(expanded, activeSort, onClickSort)
 
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
@@ -124,6 +140,85 @@ fun CategoryRow(
         }
     }
 }
+
+@Composable
+fun SortView(
+    expanded: MutableState<Boolean>,
+    activeSort: State<Int>,
+    onClickSort: (Int) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(vertical = 16.dp)
+    ) {
+        Text(
+            text = "Сортировка",
+            modifier = Modifier.align(Alignment.Start)
+        )
+        Box(
+            modifier = Modifier.align(Alignment.Start)
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(top = 4.dp)
+                    .clickable {
+                        expanded.value = true
+                    },
+                text =
+                when (activeSort.value) {
+                    0 -> "По возрастанию цены"
+                    1 -> "По убыванию цены"
+                    else -> "Отсутствует"
+                },
+                color = MaterialTheme.colorScheme.onTertiary
+            )
+            DropMenu(
+                expanded = expanded.value,
+                onSelectedClickListener = onClickSort,
+                onDismissClickListener = {
+                    expanded.value = it
+                }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun DropMenu(
+    expanded: Boolean,
+    onSelectedClickListener: (Int) -> Unit,
+    onDismissClickListener: (Boolean) -> Unit
+) {
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { onDismissClickListener(false) },
+        modifier = Modifier.background(MaterialTheme.colorScheme.tertiary)
+    ) {
+        DropdownMenuItem(
+            onClick = {
+                onSelectedClickListener(-1)
+                onDismissClickListener(false)
+            },
+            text = { Text("Отсутствует") }
+        )
+        DropdownMenuItem(
+            onClick = {
+                onSelectedClickListener(0)
+                onDismissClickListener(false)
+            },
+            text = { Text("По возрастанию цены") }
+        )
+        DropdownMenuItem(
+            onClick = {
+                onSelectedClickListener(1)
+                onDismissClickListener(false)
+            },
+            text = { Text("По убыванию цены") }
+        )
+    }
+}
+
 
 @Composable
 fun CardProduct(product: Product, navController: NavController) {
